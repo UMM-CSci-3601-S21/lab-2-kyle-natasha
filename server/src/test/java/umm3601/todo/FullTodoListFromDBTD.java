61c1cf9b8a38af9c30870d35b6ee3e0641373f59
Kyle Day <day00090@morris.umn.edu>
2021-02-13 16:17:27 -0600

    Add Filter Category Functionality and JSON Entry Test
    This adds a method and search filter for categories in DataBaseTD, a testing class to make sure the categories can be called on by the methods, and a test to FullToDoListFromDBTD.java that verifies that entries of todos.json can be called.
    Co-authored-by: Natasha A. Zebrev <zebre001@morris.umn.edu>

--- server/src/test/java/umm3601/todo/FullTodoListFromDBTD.java
+++ server/src/test/java/umm3601/todo/FullTodoListFromDBTD.java
@@ -16,4 +16,16 @@ public class FullTodoListFromDBTD {
     assertEquals(300, allTodos.length, "Incorrect total number of todos");
   }
 
+  @Test
+  public void firstUserInFullList() throws IOException {
+    DatabaseTD db = new DatabaseTD("/todos.json");
+    Todo[] allTodos = db.listTodos(new HashMap<>());
+    Todo firstTodo = allTodos[0];
+    assertEquals("58895985a22c04e761776d54", firstTodo._id, "Incorrect ID");
+    assertEquals("Blanche", firstTodo.owner, "Incorrect name");
+    assertEquals(false, firstTodo.status, "Incorrect status");
+    assertEquals("In sunt ex non tempor cillum commodo amet incididunt anim qui commodo quis. Cillum non labore ex sint esse.", firstTodo.body, "Incorrect body");
+    assertEquals("software design", firstTodo.category, "Incorrect category");
+  }
+
 }
